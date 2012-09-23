d8949a0453b756dd365bfdd3da51c9dd579230df
Marcin Łyko <marcin.g.lyko@gmail.com>
2012-09-23 22:20:30 +0200

    Add license header to all files

--- jsonrpc2/errors.py
+++ jsonrpc2/errors.py
@@ -1,6 +1,29 @@
+# This file is part of Json-RPC2.
+#
+# Copyright (C) 2012 Marcin Lyko
+# All rights reserved.
+#
+# Json-RPC2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Json-RPC2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Json-RPC2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+'''
+Definitions of Json-RPC exception classes.
+'''
+
 class JsonRpcError(Exception):
     '''
-    A base class of Json-RPC errors
+    A base class of Json-RPC errors.
     '''
     def __init__(self, code=32000, message='JSON-RPC error.',
                       id=None, data=None):
--- jsonrpc2/server.py
+++ jsonrpc2/server.py
@@ -1,3 +1,26 @@
+# This file is part of Json-RPC2.
+#
+# Copyright (C) 2012 Marcin Lyko
+# All rights reserved.
+#
+# Json-RPC2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Json-RPC2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Json-RPC2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+'''
+Definitions of Json-RPC server side classes.
+'''
+
 import sys
 import socket
 import asyncore
