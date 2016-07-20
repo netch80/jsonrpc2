9a18023d488aa2ba9776113789741ac27daa4523
Stanislav Bondarenko <gkpect@gmail.com>
2016-07-21 00:52:33 +0300

    Set SO_REUSEADDR for server socket

--- jsonrpc2/server.py
+++ jsonrpc2/server.py
@@ -220,6 +220,7 @@ class JsonRpcServer(asyncore.dispatcher):
         try:
             asyncore.dispatcher.__init__(self)
             self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.set_reuse_addr()
             self.bind(address)
             self.listen(0)
         except Exception:
